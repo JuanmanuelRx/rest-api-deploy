bd81b5ef45361b5a6a0afb630b7562c3f1d81f10
JuanmanuelRx <juanmanuelcastillovas@gmail.com>
2024-10-08 18:17:58 +0200

    first commit

--- models/mysql/movie.js
+++ models/mysql/movie.js
@@ -3,33 +3,66 @@ import mysql from 'mysql2/promise'
 const config = { 
     host: 'localhost',
     user: 'root',
+    port: 3307,
     password: 'curso_123',  // replace with your actual MySQL password
     database:'moviesdb'
 };
 const connection = await mysql.createConnection(config);
 
 // Modelo para interactuar con la base de datos MySQL
-connection.connect((err) => {
-    if (err) throw err;
-    console.log('Connected to the MySQL server.');
-})
-
-connection.end();
 export class MovieModel {
     static async getAll ({ genre }) {
-        const result = await connection.query('select * from movies');
-        console.log(result);
+        const [ movies ] = await connection.query(
+            'select BIN_TO_UUID(id) as id, title, _year, director, duration, poster, rate from movie');
+        if(genre) {
+            const loweCaseGenres = genre.toLowerCase();
+            const [ genres ] = await connection.query(
+                `SELECT * FROM genre WHERE LOWER(name) LIKE ?`, [loweCaseGenres]
+            )
+            if(genres.length === 0) return [];
+            const [{ id }] = genres;
+            
+            const [ movies_genres ] = await connection.query(
+                `SELECT * FROM movie_genres INNER JOIN movie WHERE movie_id=?`, [id]
+            );
+        }
+        return movies
+        
     }
     static async getById ({ id }) {
-        // ...
+        const [ movies ] = await connection.query(
+            'select BIN_TO_UUID(id) as id, title, _year, director, duration, poster, rate from movie where id = UUID_TO_BIN(?)', [ id ]);
+        if (movies.length === 0) return null
+        return movies[0]
     }
     static async create ({ input }) {
-        // ...
+        const {
+            title,
+            _year,
+            director,
+            duration,
+            poster,
+            rate
+        } = input;
+        const [uuidResult] = await connection.query('SELECT UUID() AS uuid')
+        const [{ uuid }] = uuidResult
+        try {
+            await connection.query(`insert into movie (id, title, _year, director, duration, poster, rate) 
+            VALUES (UUID_TO_BIN(${uuid}),?,?,?,?,?,?)`, [title, _year, director, duration, poster, rate])
+        } catch (error) {
+            console.error(error)
+            sendLog(error)
+            throw new Error('Error al crear el movie')
+        }
+        const [movies] = await connection.query(
+            `select BIN_TO_UUID(id) as id, title, _year, director, duration, poster, rate 
+            from movie where id = UUID_TO_BIN(?)`, [uuid]);
+        return movies[0]
     }
     static async delete ({ id }) {
-        // ...
+        //crear delete
     }
     static async update ({ id, input }) {
-        // ...
+        //crear update
     }
 }
